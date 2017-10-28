889f7d9e5661a5421b723184f7f303b250c44a79
Quinn Ciccoretti <quinnc423@gmail.com>
2017-10-28 19:32:24 -0400

    merged with ochome

--- js/engine.js
+++ js/engine.js
@@ -1,49 +1,98 @@
-<!DOCTYPE html>
-
-<html>
-
-<head>
-	<title>Jenga - Physijs</title>
 	
-	<link rel="stylesheet" type="text/css" href="css/styles.css" />
-	
-	<script type="text/javascript" src="js/three.min.js"></script>
-	<script type="text/javascript" src="js/stats.js"></script>
-	<script type="text/javascript" src="../physi.js"></script>
-	
-	<script type="text/javascript">
-	
+	var currentframe = 0; //most recent leap data
+	var leapcontroller; 	// for getting leap data
 	'use strict';
 	
-	Physijs.scripts.worker = '../physijs_worker.js';
-	Physijs.scripts.ammo = 'examples/js/ammo.js';
+	Physijs.scripts.worker = 'js/physijs_worker.js';
+	Physijs.scripts.ammo = 'ammo.js';
 	
-	var test_block, initScene, initEventHandling, draw_test_block, currmousex, currmousey, render, createTower, loader,
+	var test_block, draw_hands, init_world, initEventHandling, draw_test_block, currmousex, currmousey, render, createTower, loader,
 		renderer, render_stats, physics_stats, scene, dir_light, am_light, camera,
-		table, blocks = [], table_material, block_material, intersect_plane,
+		table, blocks = [], lhand, rhand,lbock,rblock,table_material, block_material, intersect_plane,
 		selected_block = null, mouse_position = new THREE.Vector3, block_offset = new THREE.Vector3, _i, _v3 = new THREE.Vector3;
+	// var boneboxes = [];
+	var boneMeshes;
+ //    var jointMeshes = [];
+ 	console.log("----------------------");
+		for (var i = 0; i < 54; i++) {
+			boneMeshes.push(new Physijs.SphereMesh(
+		        new THREE.SphereGeometry(1),
+		        Physijs.createMaterial(new THREE.MeshBasicMaterial({color: 0xff0000}), 0, 1),
+		        1
+		    ));
+		}
+ 	console.log("BoneMeshes");
+		console.log(boneMeshes);
+	//from effect demo
+	var realcamera;
+	var guiVisible = true;
+	var mesh, effect, controls, oculuscontrol;
+	var clock = new THREE.Clock();
 	var mousehasbeenmoved = false;
 	
-	initScene = function() {
+	//init_all();		var bmesh = [];
+		
+		
+	function init_leap() {
+		console.log("Initialized Leap");
+		leapcontroller = new Leap.Controller({enableGestures: true})
+		.use('screenPosition')
+		.connect()
+		.on('frame', function(frame){
+			//console.log(frame);	//will get a frame, no need to check
+			currentframe = frame; //update a reference to the current frame for use in setting in the phys sim
+			//lhand = currentframe.
+			console.log(currentframe);
+			
+		})
+		
+
+
+	}	
+	function init_renderer(){
+		//set renderer
 		renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		renderer.shadowMap.enabled = true;
 		renderer.shadowMapSoft = true;
 		document.getElementById( 'viewport' ).appendChild( renderer.domElement );
-		
+	}
+	function init_camera(){
+		//Init camera ////////////////////
+		camera = new THREE.PerspectiveCamera(
+			35,
+
+			window.innerWidth / window.innerHeight,
+			1,
+			1000
+		);
+		camera.position.set( 25, 20, 25 );
+		camera.lookAt(new THREE.Vector3( 0, 7, 0 ));
+	}
+	function init_stats (argument) {
+		//init stats module
 		render_stats = new Stats();
 		render_stats.domElement.style.position = 'absolute';
 		render_stats.domElement.style.top = '1px';
 		render_stats.domElement.style.zIndex = 100;
 		document.getElementById( 'viewport' ).appendChild( render_stats.domElement );
-
+		//init stats module
 		physics_stats = new Stats();
 		physics_stats.domElement.style.position = 'absolute';
 		physics_stats.domElement.style.top = '50px';
 		physics_stats.domElement.style.zIndex = 100;
 		document.getElementById( 'viewport' ).appendChild( physics_stats.domElement );
-		
+	}
+	function init_effect(){
+		//Added oculus effect ////////////////////////////////////
+		effect = new THREE.OculusRiftEffect( renderer, { worldScale: 1 } );
+		effect.setSize( window.innerWidth, window.innerHeight );
+	}
+	function init_scene(){
 		scene = new Physijs.Scene({ fixedTimeStep: 1 / 120 });
+		scene.add( camera ); //order is weir d
+		//var scene = Leap.loopController.plugins.boneHand.scene;
+		// setting up
 		scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
 		scene.addEventListener(
 			'update',
@@ -67,15 +116,7 @@
 			}
 		);
 		
-		camera = new THREE.PerspectiveCamera(
-			35,
-			window.innerWidth / window.innerHeight,
-			1,
-			1000
-		);
-		camera.position.set( 25, 20, 25 );
-		camera.lookAt(new THREE.Vector3( 0, 7, 0 ));
-		scene.add( camera );
+		
 		
 		// ambient light
 		am_light = new THREE.AmbientLight( 0x444444 );
@@ -102,7 +143,7 @@
 		
 		// Materials
 		table_material = Physijs.createMaterial(
-			new THREE.MeshLambertMaterial({ map: loader.load( 'images/wood.jpg' )}),
+			new THREE.MeshLambertMaterial({ map: loader.load( 'images/mcafee.jpg' )}),
 			.9, // high friction
 			.2 // low restitution
 		);
@@ -136,14 +177,88 @@
 		);
 		intersect_plane.rotation.x = Math.PI / -2;
 		scene.add( intersect_plane );
+	}
+	init_world = function() {
+		init_renderer();
+		init_camera();
+		init_effect();
 
-		initEventHandling();
+		controls = new THREE.FirstPersonControls( camera );
+		controls.movementSpeed = 40;
+		controls.lookSpeed = 0.1;
+		controls.lookVertical = true;
+		oculuscontrol = new THREE.OculusControls( camera );
 		
+		init_stats();
+
+		init_scene();
+		
+		window.addEventListener( 'resize', onWindowResize, false );
+		document.addEventListener('keydown', keyPressed, false);
+		oculuscontrol.connect();//presumably engages oculus tracking
+		initEventHandling();
 		requestAnimationFrame( render );
 		scene.simulate();
 	};
-
+	//from the effect demo
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		// realcamera.aspect = window.innerWidth / window.innerHeight;
+		//realcamera.updateProjectionMatrix();
+		effect.setSize( window.innerWidth, window.innerHeight );
+		controls.handleResize();
+	}
 	
+	draw_hands = function(){
+		if(currentframe == 0){
+			return 0;
+		}
+		//console.log(currentframe);
+		var dat = [];
+		fingers = currentframe.fingers;
+		var count = 0;
+		for (var i = 0; i < fingers.length; i++) {
+			for (var j = 0; j < fingers[i].bones.length; j++) {
+				var b = fingers[i].bones[j];
+				var center = b.center();
+				console.log("BoneMeshes");
+				console.log(boneMeshes);
+				console.log(center);
+				var cmesh = boneMeshes[count];
+				cmesh.position.set( 0, 0, 0 );
+		   		cmesh.__dirtyPosition = true;
+				// You may also want to cancel the object's velocity
+				cmesh.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+				cmesh.setAngularVelocity(new THREE.Vector3(0, 0, 0));
+
+			}
+		}
+		console.log(boneMeshes);
+		var hands = currentframe.hands;
+		for (var i = 0; i < boneMeshes.length; i++) {
+			var mesh = boneMeshes[i];
+			mesh.position.set( 0, 0, 0 );
+		    mesh.__dirtyPosition = true;
+
+		    // Change the object's rotation
+		    mesh.rotation.set(0, 90, 180);
+		    mesh.__dirtyRotation = true;
+		    
+		    // You may also want to cancel the object's velocity
+		    mesh.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+		    mesh.setAngularVelocity(new THREE.Vector3(0, 0, 0));
+		};
+		
+		
+
+
+	}
+	function draw_h(hand){
+		
+		
+		
+	}
 	draw_test_block = function(){
 		var cvec = new THREE.Vector3;
 		cvec.set(
@@ -152,7 +267,7 @@
 					1
 				);
 				cvec.unproject( camera );
-				console.log(cvec);
+				//console.log(cvec);
 
 				var scalex = 20;
 				var scaley = 20;
@@ -160,34 +275,26 @@
 				var xpos = cvec.x/scalex+40;
 				var ypos = cvec.y/scaley+40;
 				var zpos = cvec.z/scalez +20;
-				
-
-			    //cvec.set( 0, 0, 0 );
-				// test_block.setAngularFactor( cvec );
 				if(mousehasbeenmoved){
-					// console.log("Pos")
-					// console.log(test_block.position);
-					// var lv = new THREE.Vector3(test_block.position.x - xpos, test_block.position.y - ypos,test_block.position.z - zpos);
-					// console.log(lv);
-					// //test_block.setAngularVelocity( cvec );
-					// //test_block.setLinearFactor( cvec );
-					// test_block.setLinearVelocity( lv );
-				 //    // You may also want to cancel the object's velocity
-				 //    //test_block.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-					// test_block.setAngularVelocity(new THREE.Vector3(0, 0, 0));
-
+				
 					test_block.position.set(xpos, ypos, zpos);
 				    test_block.__dirtyPosition = true;
-				    console.log(test_block.position);
+				    //console.log(test_block.position);
 				    // Change the object's rotation
 				    test_block.rotation.set(0, 0, 0);
 				    test_block.__dirtyRotation = true;
 				}
 	}
 	render = function() {
-		draw_test_block();
+		draw_hands();
+		//draw_test_block();
 		requestAnimationFrame( render );
-		renderer.render( scene, camera );
+		//renderer.render( scene, camera );
+		//effect
+		//var t = clock.getElapsedTime();
+		controls.update( clock.getDelta() );		//Necessary for movement
+		oculuscontrol.update( clock.getDelta() );	//Necessary for movement
+		effect.render( scene, camera );
 		render_stats.update();
 	};
 	
@@ -196,7 +303,7 @@
 			block_geometry = new THREE.BoxGeometry( block_length, block_height, block_width );
 		
 		return function() {
-			var i, j, rows = 20,
+			var i, j, rows = 200,
 				block;
 			
 			for ( i = 0; i < rows; i++ ) {
@@ -215,10 +322,7 @@
 					blocks.push( block );
 				}
 			}
-		test_block = new Physijs.BoxMesh( block_geometry, block_material );
-		test_block.receiveShadow = true;
-		test_block.castShadow = true;
-		scene.add( test_block );
+			
 		}
 	})();
 	
@@ -298,17 +402,3 @@
 		};
 	})();
 	
-	window.onload = initScene;
-	
-	</script>
-</head>
-
-<body>
-	<div id="heading">
-		<h1>Jenga</h1>
-		<p>Click & drag on the blocks to move them around.</p>
-	</div>
-	<div id="viewport"></div>
-</body>
-
-</html>
