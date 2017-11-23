eba92036538daf06f444989fe41038ad7879d9f8
Quinn Ciccoretti <qciccoretti@quinn.fedoralaptop>
2017-11-23 10:11:26 -0500

    "added sh init file"

--- gitcom.sh
+++ gitcom.sh
@@ -2,3 +2,4 @@ git add -A
 read -p "Enter commit message: "  msg
 git commit -m "$msg"
 git push origin master
+echo "All done!"
---
+++ resinit.sh
@@ -0,0 +1,5 @@
+echo --------------------------
+echo      Initializing...      
+echo --------------------------
+python3 -m http.server 8000
+echo "Init Completed"
