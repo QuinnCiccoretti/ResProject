974d0d9e09b9534209764d6f5cfe77df244d7153
Quinn Ciccoretti <quinnc423@gmail.com>
2018-01-02 15:57:56 -0500

    added nodejs serve and procfile

--- js/serve.js
+++
@@ -1,6 +0,0 @@
-var app = require('express')();
-var http = require('http').Server(app);
-// var roomname = 'DEFAULT_ROOM'
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
