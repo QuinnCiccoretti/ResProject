77888eb21ea33a2842779eafc66eb24a982c03fe
Quinn Ciccoretti <quinnc423@gmail.com>
2018-01-02 15:29:14 -0500

    added heroku

--- js/serve.js
+++ js/serve.js
@@ -1,10 +1,5 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var roompasses = {};
-function valid(roomname,key){
-  return roompasses[roomname][0] == key;
-}
 // var roomname = 'DEFAULT_ROOM'
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
