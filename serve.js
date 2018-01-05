ec92367caa67ecfa1bfe253dd64374dc652c7eed
Quinn Ciccoretti <quinnc423@gmail.com>
2018-01-05 11:26:57 -0500

    changed procfile to start frontpage

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/front.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
