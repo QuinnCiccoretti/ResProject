2a55e737ba465e816ea3a31bde34bd351523b6ac
Quinn Ciccoretti <quinnc423@gmail.com>
2018-01-05 11:29:43 -0500

    changed procfile to start index

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/front.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
