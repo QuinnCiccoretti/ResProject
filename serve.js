452aea81bf1a96e31e10a01982bcc6c8f3f4bb57
Quinn Ciccoretti <quinnc423@gmail.com>
2018-01-16 17:50:24 -0500

    changed port

--- serve.js
+++ serve.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
